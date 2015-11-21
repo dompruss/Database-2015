be312bcca3ac18d29cebd39bcc448e72edff049b
Nicolas <Nicolas@40pieceKFC>
2015-11-21 16:35:01 -0700

    data

--- src/data_rw/Application.java
+++ src/data_rw/Application.java
@@ -14,17 +14,17 @@ public class Application {
         String caseFile = "CASE_Data.txt"; //done
         String moboFile = "MOBO_Data.txt"; //done
         String cpuFile = "CPU_Data.txt"; //done
-        String heatSinkFile = "HEATSINK_Data.txt"; //
-        String gpuFile = "GPU_Data.txt";
         String ramFile = "RAM_Data.txt"; //done
+        String heatSinkFile = "HEATSINK_Data.txt";
+        String gpuFile = "GPU_Data.txt";
         String hardDriveFile = "HARDDRIVE_Data.txt";
-        String powerSupplyFile = "POWERSUPPLY.txt";
+        String powerSupplyFile = "PSU_Data.txt";
         
        // DataWriter caseF = new CaseWriter();
         
-        DataReader drCASE = new DataReader(caseFile, "case");
-        DataReader drMOBO = new DataReader(moboFile, "mobo");
-        DataReader drCPU = new DataReader(cpuFile, "cpu");
+        //DataReader drCASE = new DataReader(caseFile, "case");
+        //DataReader drMOBO = new DataReader(moboFile, "mobo");
+        //DataReader drCPU = new DataReader(cpuFile, "cpu");
         DataReader drRAM = new DataReader(ramFile, "ram");
     }
 }
